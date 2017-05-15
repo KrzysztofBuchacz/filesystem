61aa1e52814e0655f71dfc3cef4c69d56a67728f
Krzysztof Buchacz <Krzysztof.Buchacz@autodesk.com>
2017-05-15 16:35:22 +0200

    directory_iterator implemented

--- kbu/filesystem.cpp
+++ kbu/filesystem.cpp
@@ -1,5 +1,12 @@
 #include "filesystem.h"
+#include <string.h>
+#ifdef _WIN32
+const char sep[] = "\\";
+#else
+const char sep[] = "/";
 #include <sys/stat.h>
+#include <dirent.h>
+#endif
 
 using namespace kbu::filesystem;
 
@@ -9,7 +16,11 @@ path::path(const std::string &p) : _path(p)
 
 path path::extension() const
 {
-    return _path;
+    size_t s = _path.rfind(sep);
+    size_t d = _path.rfind(".");
+    if (d > s)
+        return _path.substr(d);
+    return path(string());
 }
 
 std::string path::string() const
@@ -34,6 +45,22 @@ const kbu::filesystem::path & directory_entry::path() const
 std::vector<directory_entry> kbu::filesystem::directory_iterator(const path &path)
 {
     std::vector<directory_entry> ret;
+    DIR *dir = opendir(path.string().c_str());
+    if (dir)
+    {
+        if (struct dirent *entry = readdir(dir))
+        {
+            do
+            {
+                if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+                    continue;
+                kbu::filesystem::path p(path.string() + sep + entry->d_name);
+                ret.push_back(p);
+            }
+            while ((entry = readdir(dir)) != nullptr);
+        }
+        closedir(dir);
+    }
     return ret;
 }
 
